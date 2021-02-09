9603ef8496a1f7a21d3725c51eab07d488c88ed2
AlphaArtrem <yashawasthi01@gmail.com>
2021-02-09 16:51:11 +0530

    Filter Table creation

--- index.js
+++ index.js
@@ -3,15 +3,35 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/src/html/index.html'));
-  //__dirname : It will resolve to your project folder.
+const fs = require('fs');
+const credsFile = fs.readFileSync('creds.json');
+const creds = JSON.parse(credsFile);
+
+const { Client } = require('pg');
+
+const client = new Client(creds.redshift);
+
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
 });
 
-app.get('/test',(req,res)=>{
-    //res.sendFile(__dirname +"/views/test.html",);
-    res.json({title:"api",message:"root"});
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/src/html/index.html'));
+});
+
+app.get('/test', async (req,res) => {
+    const query = "SELECT * FROM es_articles_test LIMIT 5";
+    client.query(query, function(err, result) {
+      if(err) {
+        res.json({error : `error running query : ${err}`});
+      }
+      res.json({result : result});
+    });    
 })
+
+
 
 //add the router
 app.use('/', router);
--- src/js/load.js
+++ src/js/load.js
@@ -1,12 +1,3 @@
-/*let url="http://localhost:3000/test";
-fetch(url).then(response => response.json())
-.then( (result) => {
-    console.log('success:', result)
-    let div=document.getElementById('test');
-    div.innerHTML=`title: ${result.title}<br/>message: ${result.message}`;
-})
-.catch(error => console.log('error:', error));*/
-
 const filters = ["date", "sport", "entity", "writer", "publishedTime", "editor"];
 let filterValues = {};
 let filterBubbles = document.getElementById("filters")
@@ -17,28 +8,79 @@ for(let i = 0; i < filters.length; i++){
         filterValues[filters[i]] = [filterDropdown.value ,`
         <div class="col-sm-auto" style="background-color: white; margin-right: 1%">
             <div class="col-sm">
-            <div class="row">
-                <div class="col-sm-auto">
-                ${filterDropdown.value}
+                <div class="row">
+                    <div class="col-sm-auto">
+                    ${filterDropdown.value}
+                    </div>
+                    <div class="col-sm-auto">
+                    <button class="btn" style="padding: 0%;" onClick="removeFilter(${filters[i]})"><i class="fa fa-close"></i></button>
+                    </button>
+                    </div>
                 </div>
-                <div class="col-sm-auto">
-                <button class="btn" style="padding: 0%;" onClick="removeFilter(${filters[i]})"><i class="fa fa-close"></i></button>
-                </button>
-                </div>
-            </div>
             </div>
         </div>`];
         filterBubbles.innerHTML = "";
         for(let filter in filterValues){
             filterBubbles.innerHTML += filterValues[filter][1];
         }
+
+        if(lenghtOfObject(filterValues) <= 6){
+            filterBubbles.innerHTML += `
+            <div class="col-sm-auto">
+                <button class="btn btn-dark" style="padding: 3% 20% 3% 20%;" onClick="showFilteredData()"">Filter</button>
+            </div>
+           `;
+        }
     });
 }
 
 const removeFilter = (filterName) => {
-    delete filterValues[filterName.id];
+    delete filterValues[filterName];
     filterBubbles.innerHTML = "";
     for(let filter in filterValues){
         filterBubbles.innerHTML += filterValues[filter][1];
     }
 };
+
+const lenghtOfObject = (object) =>{
+    let length = 0;
+    for(let _ in object){
+        length++;
+    }
+    return length;
+}
+
+const showFilteredData = () => {
+    let url = `http://${window.location.host}/test?`;
+    for(let filter in filterValues){
+        url += filter + "=" + filterValues[filter][0] + '&';
+    }
+    console.log(url);
+    fetch(url).then(response => response.json())
+    .then( (result) => {
+        if("error" in result){
+            console.log(result);
+        }else{
+            console.log(result);
+            document.getElementById("filter-table").style.visibility = "visible";
+            let tableData = document.getElementById("filter-table-body");
+            tableData.innerHTML = "";
+            for(const data in result.result.rows){
+                const dateTime = new Date(result.result.rows[data].timestamp_ist);
+                tableData.innerHTML += `
+                <tr>
+                    <td>${dateTime.getDate()}/${dateTime.getMonth()}/${dateTime.getFullYear()}</td>
+                    <td>${result.result.rows[data].sports}</td>
+                    <td>${result.result.rows[data].entities}</td>
+                    <td>${result.result.rows[data].author}</td>
+                    <td>${dateTime.getTime()}</td>
+                    <td>${result.result.rows[data].editor}</td>
+                    <td>${result.result.rows[data].word_count}</td>
+                    <td>${result.result.rows[data].youtube_videos_count}</td>
+                </tr>
+                `;
+            }
+        }
+    })
+    .catch(error => console.log('error:', error));
+}
